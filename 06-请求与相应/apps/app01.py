42089d23de69d7f9e6cd023a6641f590d0a2ae23
小潘 <1141841975@qq.com>
2025-08-06 09:27:55 +0800

    请求体数据

---
+++ 06-请求与相应/apps/app01.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+app01 = APIRouter()
+
+@app01.get("/user/{id}")
+def get_user(id):
+    print(id)
+    return {
+        "user_id": "id"
+    }
---
+++ 06-请求与相应/apps/app02.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from typing import Union, Optional
+
+app02 = APIRouter()
+
+@app02.get("/jobs")
+async def get_jobs(kd, xl: Union[str,None], gj: Optional[str] = None):   #有默认参数的话代表着可以填可以不填
+    # 在里面的是路径参数,不在里面的都算是查询参数
+    # 基于kd, xl, gj数据库查询岗位信息
+    return {
+        "kd": kd,
+        "xl": xl,
+        "gj": gj
+    }
