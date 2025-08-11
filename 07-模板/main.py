7115415b508a4456a0888dc71354f79ca4b171dd
小潘 <1141841975@qq.com>
2025-08-11 10:57:58 +0800

    jinja2 模板语法-变量渲染

---
+++ 07-模板/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Request
+import uvicorn
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/index")
+def index(request: Request):
+    name = "root"
+    age = 32
+
+    books = ["三国演义", "水浒传", "西游记", "红楼梦"]
+
+    info = {"name": "rain", "age": 32, "gender": "male"}
+
+    return templates.TemplateResponse(
+        "index.html",      # 模板文件
+        {
+            "request": request,
+            "user": name,
+            "age": age,
+            "books": books,
+            "info": info,
+         },     # context上下文对象,一个字典
+    )
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "main:app",
+        port=8090,
+        reload=True
+    )
